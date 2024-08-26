df94d193e8348c1365175d43b4810d0bdafdeef5
Guilherme Henrique <guilherme_h2006@hotmail.com>
2024-08-26 09:06:12 -0300

    consulta componente para api

--- Front/NunesSport-App/src/app/app.component.ts
+++ Front/NunesSport-App/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProdutosComponent } from "./produtos/produtos.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProdutosComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
